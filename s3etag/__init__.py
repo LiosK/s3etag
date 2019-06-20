9a1c9450e0960ccc1754d000cb1d43daa54a9f91
LiosK <contact@mail.liosk.net>
2019-06-21 08:32:20 +0900

    Release v0.1.0
    - Achieve initial operating capability.

---
+++ s3etag/__init__.py
@@ -0,0 +1,59 @@
+# vim: set fileencoding=utf-8 :
+
+import argparse
+import hashlib
+import re
+import sys
+
+def main():
+    # parse arguments
+    parser = argparse.ArgumentParser(description="s3etag - compute AWS S3 Etags")
+    parser.add_argument("files", nargs="+", help="filenames")
+    parser.add_argument(
+            "--chunksize", metavar="size", default="8MB", type=parse_chunksize,
+            help="multipart_chunksize used for upload in bytes or with a size suffix KB, MB, GB, or TB (default: 8MB)",
+            )
+    args = parser.parse_args()
+
+    # loop given files
+    results = [ process_file(f, args.chunksize) for f in args.files ]
+    return int(any(results))
+
+
+def parse_chunksize(size):
+    """Parse chunksize argument"""
+    match = re.fullmatch(r"(\d+)([KMGT]B)?", size)
+    if match is None:
+        raise argparse.ArgumentTypeError(f"invalid size value: '{size}'")
+    num, suffix = match.groups("")
+    return int(num) * ({
+        "": 1,
+        "KB": 1024 ** 1,
+        "MB": 1024 ** 2,
+        "GB": 1024 ** 3,
+        "TB": 1024 ** 4,
+        }[suffix])
+
+
+def process_file(filename, chunksize):
+    """Compute and print Etag for a file"""
+    count = 0
+    dgst_part = hashlib.md5()
+    dgst_whole = hashlib.md5()
+    try:
+        with open(filename, "rb") as fh:
+            while True:
+                buf = fh.read(chunksize)
+                if len(buf) < 1:
+                    break
+                count = count + 1
+                dgst_part = hashlib.md5(buf)
+                dgst_whole.update(dgst_part.digest())
+    except OSError as err:
+        print(f"ERROR: {err}", file=sys.stderr, flush=True)
+        return err.errno
+
+    etag = f"{dgst_whole.hexdigest()}-{count}" if count > 1 else dgst_part.hexdigest()
+    print(f"{etag: <40s}{filename}", flush=True)
+
+    return 0
---
+++ setup.py
@@ -0,0 +1,20 @@
+# vim: set fileencoding=utf-8 :
+
+import setuptools
+
+setuptools.setup(
+        name="s3etag",
+        version="0.1.0",
+        description="Compute AWS S3 Etags.",
+        author="LiosK",
+        author_email="contact@mail.liosk.net",
+        url="https://github.com/LiosK/s3etag",
+        packages=setuptools.find_packages(),
+        entry_points={
+            "console_scripts": ["s3etag=s3etag:main"],
+            },
+        classifiers=[
+            "License :: OSI Approved :: Apache Software License",
+            "Programming Language :: Python :: 3 :: Only",
+            ],
+        )
