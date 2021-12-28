13323235dcb0e5b98e62ee25d893fb80b739f84c
LiosK <contact@mail.liosk.net>
2021-12-28 09:22:58 +0900

    v0.1.3 Released
    Changed:
    
    - Build tool from setuptools to Poetry.
    - Code style to black's.

--- s3etag/__init__.py
+++ s3etag/__init__.py
@@ -5,18 +5,22 @@ import hashlib
 import re
 import sys
 
+
 def main():
     # parse arguments
     parser = argparse.ArgumentParser(description="s3etag - compute AWS S3 Etags")
     parser.add_argument("files", nargs="+", help="filenames")
     parser.add_argument(
-            "--chunksize", metavar="size", default="8MB", type=parse_chunksize,
-            help="multipart_chunksize used for upload in bytes or with a size suffix KB, MB, GB, or TB (default: 8MB)",
-            )
+        "--chunksize",
+        metavar="size",
+        default="8MB",
+        type=parse_chunksize,
+        help="multipart_chunksize used for upload in bytes or with a size suffix KB, MB, GB, or TB (default: 8MB)",
+    )
     args = parser.parse_args()
 
     # loop given files
-    results = [ process_file(f, args.chunksize) for f in args.files ]
+    results = [process_file(f, args.chunksize) for f in args.files]
     return int(any(results))
 
 
@@ -26,13 +30,15 @@ def parse_chunksize(size):
     if match is None:
         raise argparse.ArgumentTypeError("invalid size value: '{}'".format(size))
     num, suffix = match.groups("")
-    return int(num) * ({
-        "": 1,
-        "KB": 1024 ** 1,
-        "MB": 1024 ** 2,
-        "GB": 1024 ** 3,
-        "TB": 1024 ** 4,
-        }[suffix])
+    return int(num) * (
+        {
+            "": 1,
+            "KB": 1024 ** 1,
+            "MB": 1024 ** 2,
+            "GB": 1024 ** 3,
+            "TB": 1024 ** 4,
+        }[suffix]
+    )
 
 
 def process_file(filename, chunksize):
@@ -53,7 +59,11 @@ def process_file(filename, chunksize):
         print("ERROR: {}".format(err), file=sys.stderr, flush=True)
         return err.errno
 
-    etag = "{}-{}".format(dgst_whole.hexdigest(), count) if count > 1 else dgst_part.hexdigest()
+    etag = (
+        "{}-{}".format(dgst_whole.hexdigest(), count)
+        if count > 1
+        else dgst_part.hexdigest()
+    )
     print("{: <39s} {}".format(etag, filename), flush=True)
 
     return 0
