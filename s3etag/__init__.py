6ece65f6824736caba8cd203db0cad96ccadfd2a
LiosK <contact@mail.liosk.net>
2022-08-11 14:32:06 +0900

    v0.1.4 Released
    - Added type hints and refactored code.

--- s3etag/__init__.py
+++ s3etag/__init__.py
@@ -1,12 +1,14 @@
 # vim: set fileencoding=utf-8 :
 
+from __future__ import annotations
+
 import argparse
 import hashlib
 import re
 import sys
 
 
-def main():
+def main() -> int:
     # parse arguments
     parser = argparse.ArgumentParser(description="s3etag - compute AWS S3 Etags")
     parser.add_argument("files", nargs="+", help="filenames")
@@ -20,11 +22,19 @@ def main():
     args = parser.parse_args()
 
     # loop given files
-    results = [process_file(f, args.chunksize) for f in args.files]
-    return int(any(results))
+    n_errors = 0
+    for filename in args.files:
+        try:
+            etag = compute_etag(filename, args.chunksize)
+            print("{: <39s} {}".format(etag, filename), flush=True)
+        except OSError as err:
+            print("ERROR: {}".format(err), file=sys.stderr, flush=True)
+            n_errors += 1
+
+    return int(n_errors > 0)
 
 
-def parse_chunksize(size):
+def parse_chunksize(size: str) -> int:
     """Parse chunksize argument"""
     match = re.fullmatch(r"(\d+)([KMGT]B)?", size)
     if match is None:
@@ -33,37 +43,28 @@ def parse_chunksize(size):
     return int(num) * (
         {
             "": 1,
-            "KB": 1024 ** 1,
-            "MB": 1024 ** 2,
-            "GB": 1024 ** 3,
-            "TB": 1024 ** 4,
+            "KB": 1024**1,
+            "MB": 1024**2,
+            "GB": 1024**3,
+            "TB": 1024**4,
         }[suffix]
     )
 
 
-def process_file(filename, chunksize):
-    """Compute and print Etag for a file"""
-    count = 0
-    dgst_part = hashlib.md5()
-    dgst_whole = hashlib.md5()
-    try:
-        with open(filename, "rb") as fh:
-            while True:
-                buf = fh.read(chunksize)
-                if len(buf) < 1:
-                    break
-                count = count + 1
-                dgst_part = hashlib.md5(buf)
-                dgst_whole.update(dgst_part.digest())
-    except OSError as err:
-        print("ERROR: {}".format(err), file=sys.stderr, flush=True)
-        return err.errno
+def compute_etag(filename: str, chunksize: int) -> str:
+    """Compute Etag for a file"""
+    with open(filename, "rb") as fh:
+        buf = fh.read(chunksize)
+        dgst_part = hashlib.md5(buf)
+        buf = fh.read(chunksize)
+        if len(buf) < 1:
+            return dgst_part.hexdigest()
 
-    etag = (
-        "{}-{}".format(dgst_whole.hexdigest(), count)
-        if count > 1
-        else dgst_part.hexdigest()
-    )
-    print("{: <39s} {}".format(etag, filename), flush=True)
-
-    return 0
+        count = 1
+        dgst_whole = hashlib.md5(dgst_part.digest())
+        while len(buf) > 0:
+            count += 1
+            dgst_part = hashlib.md5(buf)
+            dgst_whole.update(dgst_part.digest())
+            buf = fh.read(chunksize)
+        return "{}-{}".format(dgst_whole.hexdigest(), count)
